61445d43198cfc2ef99c2da24b9ecfc62b2d244c
tanmodi <moditanmay09@gmail.com>
2024-02-11 23:43:30 +0530

    pinecone updated

--- pinecone/pinecone_DB_1.js
+++ pinecone/pinecone_DB_1.js
@@ -81,6 +81,7 @@ index.upsert([
     values: r.embedding,
     metadata: {
       name: "Tanmay",
+      mobile_no : "9205875182",
       email: "tanmaymodi64@gmail.com",
       linkedin: "https://www.linkedin.com/in/tanmodi",
       github: "https://github.com/tanmodi",
